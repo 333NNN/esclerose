9c560429aa148385434057d672fed42efd474a48
333NNN <nata.cunha@aluno.senai.br>
2023-12-06 10:18:15 -0300

    foto

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Profile() {
   return (
     <div id="profile">
-      <img src="/assets/avatar2.webp" alt="Logo de Esclerose" />
+      <img src="/assets/avatar2.webp" alt="Logo de Esclerose" loading="lazy" />
       <p>@esclerosepunk</p>
     </div>
   );
